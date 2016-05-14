3a96ea77060b89ef6b0973e2715359d8e33202ec
Damien Jiang <jiangtyd@gmail.com>
2016-05-13 23:02:55 -0700

    finish PlayerRepository, add EventRepository

--- smashboards/db.py
+++ smashboards/db.py
@@ -2,6 +2,7 @@ import mysql.connector
 from mysql.connector import errorcode
 from config.config import Config
 from model import PlayerInfo, EventInfo
+from datetime import date
 
 def get_connection():
   config = Config()
@@ -57,15 +58,38 @@ class PlayerRepository(Repository):
     ) + ';'
 
   # players = list of PlayerInfo
-  def save_players_at_event(self, players, event):
+  def save_players(self, players):
     __check_list_type(players, PlayerInfo)
-    assert isInstance(event, EventInfo)
 
     player_id_name = [(player.swf_player_id, player.swf_player_name)
       for player in players]
     self.execute_statement(__insert_player_statement, player_id_name)
 
-    player_tags = [(player.swf_player_id, tag, event.swf_event_id) for tag in player.tags for player in players]
+    player_tags = [(player.swf_player_id, player.tags[event_id], event_id) for event_id in player.tags for player in players]
     self.execute_statement(__insert_player_tag_map_statement, player_tags)
 
     player_characters = [(player.swf_player_id, character) for character in player.characters for player in players]
+    self.execute_statement(__insert_player_character_statement, player_characters)
+
+class EventRepository(Repository):
+  def __init__(cnx):
+    super(EventRepository, self).__init__(cnx)
+
+  def __insert_event_statement(self, n):
+    assert isInstance(n, int) and n > 0
+    return ' '.join(
+      ['INSERT INTO `events` (`id`, `event_name`, `category`, `event_date`) VALUES']
+      + ['(%d, %ls, %s, %s)' for i in xrange(n)]
+      # if a SWF user changes his/her username
+      + ['ON DUPLICATE KEY UPDATE']
+      + ['`event_name`=VALUES(`event_name`),']
+      + ['`category`=VALUES(`category`),']
+      + ['`event_date`=VALUES(`event_date`),']
+    ) + ';'
+
+  def save_events(self, events):
+    __check_list_type(players, EventInfo)
+
+    event_info_row = [(event.swf_event_id, event.swf_event_name, event.category, event.date) for event in events]
+    self.execute_statement(__insert_event_statement, event_info_row)
+
